189b2bbc660a62639add6fd128922fe40a546426
krish singh <singjneetu5@gmail.com>
2026-09-24 18:30:17 +0530

    Create HomeController.cs

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System.Web.Mvc;
+
+namespace TasteHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
