a40eafabb2ac12eb0b1f1c1aee54cf33b0b6e7af
krish singh <singjneetu5@gmail.com>
2026-09-24 16:56:46 +0530

    Add Food model with properties

---
+++ Models/food.cs
@@ -0,0 +1,15 @@
+namespace FoodHub.Models
+{
+    public class Food
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+    }
+}
